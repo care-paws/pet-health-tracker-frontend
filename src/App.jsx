80970c507314442476390c514b39570a0a9a08fd
noelia <noelia.m.garcia98@gmail.com>
2025-12-10 10:56:44 -0300

    subo mas pantallas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
-import HealthEventForm from "./HealthEventForm";
+import CreatePetLayout from "./layouts/CreatePetLayout";
+import HealthEventForm from "./layouts/HealthEventForm";
+import PetInfoLayout from "./layouts/PetInfoLayout";
+import WeightLayout from "./layouts/WeightLayout";
 
 function App() {
-  return <HealthEventForm />;
+  // return <HealthEventForm />;
+  return <WeightLayout />;
 }
 
 export default App;
