125b940147ebf8017064ddea256e1adc80885c15
rgap <r.guzmanap@gmail.com>
2025-12-11 19:28:59 -0500

    chore: update jsconfig because of a extension mistake

--- frontend/src/assets/icons/icons.js
+++ frontend/src/assets/icons/icons.js
@@ -2,6 +2,7 @@ export { default as bellIcon } from "./bell.svg";
 export { default as calendarButtonIcon } from "./calendar-button.svg";
 export { default as calendarIcon } from "./calendar.svg";
 export { default as cameraIcon } from "./camera.svg";
+export { default as hamburgerIcon } from "./hamburger.svg";
 export { default as heartButtonIcon } from "./heart-button.svg";
 export { default as heartIcon } from "./heart.svg";
 export { default as plusButtonIcon } from "./plus-button.svg";
